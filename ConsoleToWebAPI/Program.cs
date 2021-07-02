268c7fdd366943e1c61851bc1ef7f8c66dad0cb5
mmraihan <mm_raihan@live.com>
2021-07-03 00:32:00 +0600

    Add a Default Host Builder

--- ConsoleToWebAPI/Program.cs
+++ ConsoleToWebAPI/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ConsoleToWebAPI
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello world");
+            }
+
+            public static IHostBuilder CreateHostBuilder()
+            {
+            Host.CreateDefaultBuilder();
+            }
+       
+
         }
     }
-}
+
