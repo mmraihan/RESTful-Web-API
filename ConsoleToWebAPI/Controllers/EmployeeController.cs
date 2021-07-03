602b3c0552bebb4e44bb554be6ebe7fad3320edc
mmraihan <mm_raihan@live.com>
2021-07-03 21:03:08 +0600

    Action Method Return Non-primitive Type

---
+++ ConsoleToWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleToWebAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //public string GetEmployees()
+        //{
+        //    return "GetEmployeee works";
+        //}
+
+        public EmployeeModel GetEmployees() // Return Non-primitive types
+        {
+            return new EmployeeModel()
+            {
+                Id = 1,
+                Name = "Raihan"
+            };
+        }
+
+    }
+}
