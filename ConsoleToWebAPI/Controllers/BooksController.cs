ed5b735b96725722ac880ec59376bf1bd28a0497
mmraihan <mm_raihan@live.com>
2021-07-03 20:08:33 +0600

    Route Constraints: Validate the route variables

---
+++ ConsoleToWebAPI/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        /*  [Route("{id:int:min(10)}")]*/ // Route constraint add min ()
+
+        [Route("{id:int:min(10):max(100)}")] //Route constraint add min() max()
+        public string GetById(int id)
+        {
+            return "Hello int " + id;
+        }
+
+        [Route("{id}")] // by default string
+        public string GetByString(string id)
+        {
+            return "Hello String " + id;
+        }
+    }
+}
