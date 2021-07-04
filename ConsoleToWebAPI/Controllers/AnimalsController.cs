cbcf16b047d84078ff71fb41ddee8fba75935167
mmraihan <mm_raihan@live.com>
2021-07-04 12:58:36 +0600

    Return 200 Status Code

---
+++ ConsoleToWebAPI/Controllers/AnimalsController.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleToWebAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        public IActionResult GetAnimals()
+        {
+            var animals = new List<AnimalModel>()
+            {
+                new AnimalModel(){ Id=1, Name="Cat" },
+                new AnimalModel(){ Id=2, Name="Dog" }
+            };
+
+            return Ok(animals);
+        }
+    }
+}
