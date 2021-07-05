ccfaea62a4293c9a6c6437622166d564bd730486
mmraihan <mm_raihan@live.com>
2021-07-05 17:22:23 +0600

    [BindProperty] attribute

---
+++ ConsoleToWebAPI/Controllers/CountriesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        [BindProperty]
+        public string Name { get; set; }
+        [BindProperty]
+        public int Population { get; set; }
+
+        [BindProperty]
+        public int Area { get; set; }
+
+        [HttpPost("")]
+        public IActionResult AddCountry()
+        {
+            return Ok($"Name={this.Name}, Population={this.Population}, Area={this.Area}");
+        }
+    }
+}
