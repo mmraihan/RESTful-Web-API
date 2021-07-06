8db4d55dfc133e808f6a6bf4b671ab9ec2cbb671
mmraihan <mm_raihan@live.com>
2021-07-06 09:30:34 +0600

    DI-1 Working with Services without using Dependency Injection

---
+++ ConsoleToWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleToWebAPI.Model;
+using ConsoleToWebAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductRepository _productRepositoy; // connecton between repository and controller
+
+        public ProductController()
+        {
+            _productRepositoy = new ProductRepository();
+
+        }
+
+        [HttpPost("")]
+
+        public IActionResult AddProduct([FromBody] ProductModel product)
+        {
+            _productRepositoy.AddProduct(product);
+
+            var products = _productRepositoy.GetAllProducts();
+            return Ok(products);
+        }
+
+    }
+}
---
+++ ConsoleToWebAPI/Repository/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleToWebAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI.Repository
+{
+    public class ProductRepository
+    {
+        private List<ProductModel> products = new List<ProductModel>();
+        public int AddProduct(ProductModel product)
+        {
+            product.Id = products.Count + 1;
+            products.Add(product);
+            return product.Id;
+        }
+
+        public List<ProductModel> GetAllProducts()
+        {
+            return products;
+        }
+    }
+}
